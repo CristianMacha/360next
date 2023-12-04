75e971ee495d9450b21a9ce00eb96c24a286457c
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2023-12-04 18:02:10 -0500

    scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='overflow-x-hidden'>
-        <Header />
+        {/* <Header /> */}
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   )
