21bf2e30e85a19eb0517e6004fa35c8f2901269b
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2023-12-13 18:30:09 -0500

    portada home

--- src/components/home/portada.tsx
+++ src/components/home/portada.tsx
@@ -11,8 +11,8 @@ export default function Portada() {
     const handlePlayGame = () => setGame(true);
 
     return (
-        <section className="h-auto px-[16px] md:px-[40px] flex flex-col gap-20 md:gap-14 justify-start items-center py-[16px] md:pb-[40px] md:pt-[70px]">
-            <div className="flex flex-col gap-4 md:flex-row justify-between w-full items-center py-40">
+        <section className="h-auto px-[16px] md:px-[40px] flex flex-col gap-5 md:gap-14 justify-start items-center py-[16px] md:pb-[40px] md:pt-[70px]">
+            <div className="flex flex-col gap-16 md:gap-4 md:flex-row justify-between w-full items-center py-40">
                 <div className="font-primary font-bold text-[32px] leading-normal md:text-[106px]">
                     <h1 className="leading-[normal] md:leading-[85px] whitespace-nowrap"><span className='text-[#797979]'>AGENCIA DE</span> DISEÑO</h1>
                     <h1 className="leading-[normal] md:leading-[85px]">& MARKETING</h1>
@@ -70,7 +70,7 @@ export default function Portada() {
                         Virtual Solutions
                     </div>
                 </div>
-                <div className={`${styles.actions} hidden h-[50px] absolute bottom-2 right-2 left-2 rounded-[20px] md:flex items-center justify-between px-5`}>
+                <div className={`${styles.actions} h-[50px] absolute bottom-2 right-2 left-2 rounded-[20px] flex items-center justify-between px-5`}>
                     <div className='text-white'>Interactúa con nuestra mascota (A, W, S, D)</div>
                     <div className={`w-[35px] h-[35px] rounded-full bg-white flex justify-center items-center pl-[5px] cursor-pointer animate-pulse ${game ? 'hidden' : 'block'}`} onClick={handlePlayGame}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="18" viewBox="0 0 15 18" fill="none">
